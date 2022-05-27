7551188dffb730899d2e93e494058f9c773b7d8f
Joshua Näf <naefjo@ethz.ch>
2022-05-27 16:00:31 +0200

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,13 @@
 export default function Footer() {
-    
+    return (
+        <section id="footer">
+            <div
+                className="bg-gray-800 py-2"
+            >
+                <p className="w-64 mx-auto text-slate-200 font-medium font-monoplus">
+                    A project by <span className="italic">Joshua Näf</span>.
+                </p>
+            </div>
+        </section>
+    )    
 }
